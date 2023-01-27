2fd8a2e3cb51dc57a35d34def6c26a9822d059e0
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-01-27 19:49:53 +0530

    Change title and amount by using state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,21 @@
+import React, { useState} from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle]=useState(props.title);
+  const [amnt,setAmnt]= useState(props.amount);
+
+   const clickedHandler2=()=>{
+    setAmnt(100)
+   }
+
   const clickedHandler=()=>{
-    console.log('clicked!!!!!')
+    setTitle('Updated!')
+    console.log(title)
   }
   const clickedHandler1=(e)=>{
     const deleteElement=e.target.parentElement
@@ -16,11 +26,12 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
-      <ExpenseDetails amount={props.amount} />
+      <ExpenseDetails amount={amnt} />
       <button onClick={clickedHandler}>Change title</button>
       <button onClick={clickedHandler1}>Delete expense</button>
+      <button onClick={clickedHandler2}>Change amount</button>
     </Card>
   );
 };
