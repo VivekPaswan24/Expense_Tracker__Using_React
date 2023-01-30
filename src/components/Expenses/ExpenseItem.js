7a9fae21523844415775a972190e7025dff93954
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-01-30 20:02:02 +0530

    Making the app Dynamic

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+// import React, { useState} from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
@@ -6,32 +6,32 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle]=useState(props.title);
-  const [amnt,setAmnt]= useState(props.amount);
+  // const [title, setTitle]=useState(props.title);
+  // const [amnt,setAmnt]= useState(props.amount);
 
-   const clickedHandler2=()=>{
-    setAmnt(100)
-   }
+  //  const clickedHandler2=()=>{
+  //   setAmnt(100)
+  //  }
 
-  const clickedHandler=()=>{
-    setTitle('Updated!')
-    console.log(title)
-  }
-  const clickedHandler1=(e)=>{
-    const deleteElement=e.target.parentElement
-    const parent=deleteElement.parentElement
-    parent.removeChild(deleteElement)
-  }
+  // const clickedHandler=()=>{
+  //   setTitle('Updated!')
+  //   console.log(title)
+  // }
+  // const clickedHandler1=(e)=>{
+  //   const deleteElement=e.target.parentElement
+  //   const parent=deleteElement.parentElement
+  //   parent.removeChild(deleteElement)
+  // }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <ExpenseDetails amount={amnt} />
-      <button onClick={clickedHandler}>Change title</button>
+      <ExpenseDetails amount={props.amount} />
+      {/* <button onClick={clickedHandler}>Change title</button>
       <button onClick={clickedHandler1}>Delete expense</button>
-      <button onClick={clickedHandler2}>Change amount</button>
+      <button onClick={clickedHandler2}>Change amount</button> */}
     </Card>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.items.map((ele) => (
         <ExpenseItem
+          key={ele.id}
           title={ele.title}
           amount={ele.amount}
           date={ele.date}
-        ></ExpenseItem>
+        />
       ))}
     </Card>
   );
